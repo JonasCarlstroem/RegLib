108df0db4c03c4e35c3779647179cda0965735df
JonasCarlstroem <82809067+JonasCarlstroem@users.noreply.github.com>
2025-05-21 00:52:30 +0200

    c

---
+++ RegExt.cs
@@ -0,0 +1,17 @@
+using Microsoft.Win32;
+using RegLib.Collections;
+using RegLib.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RegLib
+{
+    public static class RegHelper
+    {
+        public static RegKey OpenHiveKey(string hive)
+        {
+
+        }
+    }
+}
